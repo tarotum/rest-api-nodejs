129aa245bc28f72c6146f68affe6e6f3572311aa
tarotum <onyskiv.maxim@gmail.com>
2019-03-16 20:27:48 +0200

    refactor auth middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -6,18 +6,14 @@ ENV.config();
 module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    res.status(401).json({
-      errors: ['You could not be authorized']
-    });
+    res.status(401).json('You could not be authorized');
   } else {
     try {
       const decodedToken = jwt.verify(authorization.split(' ')[1], process.env.JWT_SECRET);
       req.userInfo = decodedToken;
       next();
     } catch (error) {
-      res.status(401).json({
-        errors: ['Auth failed']
-      });
+      res.status(401).json('Auth failed');
     }
   }
 };

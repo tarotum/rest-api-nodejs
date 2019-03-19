52793f8ba01549ca351bb1485aa0dc87c60f12eb
tarotum <onyskiv.maxim@gmail.com>
2019-03-19 19:36:16 +0200

    refactor user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,23 +8,24 @@ ENV.config();
 module.exports = {
   signup: async (req, res) => {
     try {
+      // Find user with the same email
       const userExist = await User.findOne({ email: req.body.email });
       if (userExist) {
-        res.status(409).json({ error: 'User already exist.' });
+        res.status(409).json('User already exist.');
       } else {
+        // if user not found hash password and create new user
         const passwordHash = await bcrypt.hash(req.body.password, 10);
         const user = new User({
           username: req.body.username,
           email: req.body.email,
           password: passwordHash
         });
+        await user.save();
 
-        const savedUser = await user.save();
-
-        res.status(201).json({ result: `Created ${savedUser.username} user` });
+        res.status(201).json('User created');
       }
     } catch (error) {
-      res.status(500).json({ error });
+      res.status(500).json(error);
     }
   },
   signin: async (req, res) => {
@@ -47,8 +48,8 @@ module.exports = {
             );
             res
               .status(200)
-              .header('x-auth', token)
-              .json({ result: `Welcome ${user.name}`, token });
+              .header('x-auth-token', token)
+              .json(token);
           } else {
             res.status(401).json('Invalid username or password');
           }

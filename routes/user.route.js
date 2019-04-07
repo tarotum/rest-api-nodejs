ea2366dcfcb2cd2948cdef0a82a70f8785a1341e
tarotum <onyskiv.maxim@gmail.com>
2019-04-07 19:14:27 +0300

    change path for user controller

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const Router = express.Router();
 
-const UserController = require('../controllers/user.controller');
+const UserController = require('../controllers/user');
 
 Router.post('/signup', UserController.signup);
 Router.post('/signin', UserController.signin);

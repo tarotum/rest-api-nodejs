8ff9117cd780d8cd8504ac76fc359db892941bcd
tarotum <onyskiv.maxim@gmail.com>
2019-04-07 19:13:12 +0300

    change names of post controller methods

--- routes/post.route.js
+++ routes/post.route.js
@@ -7,10 +7,10 @@ const auth = require('../middleware/auth.middleware');
 const validPost = require('../middleware/validPost.middleware');
 const upload = require('../middleware/upload');
 
-const PostController = require('../controllers/post.controller');
+const PostController = require('../controllers/post');
 
-Router.get('/get/:id', PostController.getOneById);
-Router.get('/:slug', PostController.getOneBySlug);
+Router.get('/get/:id', PostController.getById);
+Router.get('/:slug', PostController.getBySlug);
 Router.get('/', PostController.getAll);
 Router.post('/create', auth, upload.single('image'), validPost, PostController.create);
 Router.put('/update/:id', auth, upload.single('image'), PostController.updateById);
